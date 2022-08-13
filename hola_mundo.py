41e5d93e288fb6c01932a79f73d4fa0516779518
ernestonik <111098308+ernestonik@users.noreply.github.com>
2022-08-13 19:03:37 -0300

    Add files via upload

---
+++ hola_mundo.py
@@ -0,0 +1,17 @@
+# Hola Mundo! [Python]
+# ---------------------------
+# Autor: Inove Coding School
+# Version: 2.0
+
+# Descripcion:
+# Programa creado para ensayar el correcto funcionamiento
+# del entorno de instalación Python
+
+
+
+print("Hola Mundo!")
+print ("Bomba plantada")
+print ("3")
+print ("2")
+print ("1")
+print ("PUM!")
